d595cf8c937bf4991cebd61c9b94ed44f478633e
github username <github email address>
2020-04-11 15:17:57 -0600

    Adding seed and including list to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,18 @@ List.destroy_all
 User.destroy_all
 
 user77 = User.create(username:"beau", password:"pj")
+user21 = User.create(username:"Mari", password:"1021")
+
 
 L1 = List.create(title: "PJ", description:"07-05", user: user77)
 L2 = List.create(title: "P", description:"07", user: user77)
+L3 = List.create(title: "P", description:"07", user: user21)
+L4 = List.create(title: "P", description:"07", user: user21)
+
 
 I1 = Item.create(title: "JP", description:"2017", list: L1)
 I2 = Item.create(title: "J", description:"05", list: L2)
+I3 = Item.create(title: "K", description:"07", list: L3)
+I4 = Item.create(title: "A", description:"03", list: L4)
+
 
