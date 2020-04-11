4b501dd5aff4cc4ba89e38fb1421e530510fe030
github username <github email address>
2020-04-10 18:45:48 -0600

    fixing user to list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
     def index 
         @item = Item.all
-        render json: {item: @item}, include: :user
+        render json: {item: @item}, include: :list
     end
     def show 
         @item = Item.find(params[:id])
